e7cbd245f8bc85180629c7824cf335f11207d28d
cyrusmp <cype1650@gmail.com>
2024-12-14 19:15:38 -0800

    reverted to back to original

--- myapp/app.js
+++ myapp/app.js
@@ -15,9 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
