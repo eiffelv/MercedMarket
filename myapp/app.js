7d33da85d5e45d132fc912bef755a3bd70227efe
eiffelv <eiffelv@users.noreply.github.com>
2024-12-16 00:20:17 -0800

    Re-add Error Handler code and view

--- myapp/app.js
+++ myapp/app.js
@@ -31,8 +31,19 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 // Catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 app.listen(port, () => {
