18aa0d4496cb2cb7f65c47687384aef98236e3ef
eiffelv <eiffelv@users.noreply.github.com>
2024-12-17 00:08:37 -0800

    Fix index.js require

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../public/db');
+const db = require('./db');
 const title_postfix = " - Merced Market";
 
 /* GET home page. */
