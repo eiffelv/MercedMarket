4c8c22e68ce9fd31254a5fcb9e4cd4a2f915a096
cyrusmp <cype1650@gmail.com>
2024-12-16 17:23:51 -0800

    Added products to product database

--- myapp/public/db.js
+++ myapp/public/db.js
@@ -13,32 +13,53 @@ const db = new sqlite3.Database(dbPath, (err) => {
       db.run(`CREATE TABLE IF NOT EXISTS user_profiles (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE
+        email TEXT NOT NULL UNIQUE,
+        cart INTEGER DEFAULT 0
       )`, (err) => {
         if (err) {
-          console.error('Error creating user_profiles table:', err.message);
+          console.error('Error creating user profile', err.message);
         } else {
-          console.log('user_profiles table created successfully.');
+          console.log('User profile created successfully!');
         }
       });
 
-      //create a table for cart items
-      db.run(`CREATE TABLE IF NOT EXISTS cart_items (
+      //create a table for store items
+      db.run(`CREATE TABLE IF NOT EXISTS products (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER,
-        item_name TEXT NOT NULL,
-        quantity INTEGER NOT NULL,
-        price REAL,
-        FOREIGN KEY (user_id) REFERENCES user_profiles(id)
-      )`, (err) => {
-        if (err) {
-          console.error('Error creating cart_items table:', err.message);
-        } else {
-          console.log('cart items table created successfully.');
+        name TEXT NOT NULL,
+        description TEXT,
+        price REAL NOT NULL
+      )`, (err) => {  
+      if (err) {
+        console.error('Error creating products table', err.message);
+      } else {
+        console.log('Products table created successfully!');
+
+        //insert store products
+        const products = [
+          {name: 'SFSU ornament', description: 'Purple ornament with school logo in the center', price: 19.95},
+          {name: 'SFSU thermas', description: 'Stainless stell thermas with SFSU logo printed on leather', price: 29.99 },
+          {name: 'SFSU pennant', description: 'Purple and yellow SFSU pennant flag', price: 39.99},
+          {name: 'SFSU mug', description: 'White mug with notable San Francisco, California icons ', price: 49.99},
+          {name: 'SFSU gator', description: 'Stuffed alligator wearing SFSU bandana', price: 59.99},
+          {name: 'SFSU tall cup', description: 'Silver cup printed with purple and yellow snowflakes and SFSU logo printed handle', price: 69.99}
+        ];
+
+        products.forEach(product => {
+          db.run(`INSERT OR IGNORE INTO products (name, description, price) VALUES (?, ?, ?)`, 
+              [product.name, product.description, product.price, product.stock], 
+              (err) => {
+                  if (err) {
+                      console.error('Error inserting product', err.message);
+                  } else {
+                      console.log(`Product '${product.name}' inserted successfully!`);
+                  }
+              });
+          });
         }
       });
     }
-  });
+});
   
   module.exports = db;
 
