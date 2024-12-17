08557f7e5747ee858eeb00bf57019208cfdb99c1
eiffelv <eiffelv@users.noreply.github.com>
2024-12-16 22:31:44 -0800

    Fix Page Titling

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,40 +1,41 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const title_postfix = " - Merced Market";
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   console.log("Serving Home Page");
-  res.render("index", { testmessage: "Express is Working!" });
+  res.render("index", { title: "Home" + title_postfix, testmessage: "Express is Working!" });
 });
 
 /* GET /about */
 router.get("/about", function (req, res, next) {
-  res.render("about");
+  res.render("about", { title: "About" + title_postfix });
 });
 
 /* GET /cart */
 router.get("/cart", function (req, res, next) {
-  res.render("cart");
+  res.render("cart", { title: "Cart" + title_postfix });
 });
 
 /* GET /login */
 router.get("/login", function (req, res, next) {
-  res.render("login");
+  res.render("login", { title: "Login" + title_postfix });
 });
 
 /* GET /register */
 router.get("/register", function (req, res, next) {
-  res.render("register");
+  res.render("register", { title: "Register" + title_postfix });
 });
 
 /* GET /product */
 router.get("/product", function (req, res, next) {
-  res.render("product");
+  res.render("product", { title: "Product" + title_postfix });
 });
 
 /* GET /payment */
 router.get("/payment", function (req, res, next) {
-  res.render("payment");
+  res.render("payment", { title: "Payment" + title_postfix });
 });
 
 module.exports = router;
